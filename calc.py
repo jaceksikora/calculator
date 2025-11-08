c64de90da8fd3445431a9ec421196a85049ae631
Jacek Sikora <jack.sikor@gmail.com>
2025-11-08 07:15:40 +0100

    enhance calc input number validation

--- calc.py
+++ calc.py
@@ -1,10 +1,34 @@
+from __future__ import annotations
+from decimal import Decimal
+from fractions import Fraction
+from typing import Union
+
+NumberLike = Union[int, float, Decimal, Fraction, bool, str]
+
 class Calculator:
-    def __init__(self, op1: float, op2: float):
-        try:
-            self.op1 = float(op1)
-            self.op2 = float(op2)
-        except (TypeError, ValueError):
-            raise TypeError(f"To muszą byś liczby")
+    def __init__(self, op1: NumberLike, op2: NumberLike):
+        self.op1 = self._validate_number_input_(op1)
+        self.op2 = self._validate_number_input_(op2)
+
+    @staticmethod
+    def _validate_number_input_(value: NumberLike) -> float:
+        if value is None:
+            raise ValueError("No value provided")
+
+        if isinstance(value, bool):
+            raise TypeError("Bool value is not a number")
+
+        if isinstance(value, (int, float, Decimal, Fraction)):
+            return float(value)
+
+        if isinstance(value, str):
+            s = value.strip()
+            try:
+                return float(s)
+            except (ValueError, TypeError):
+                raise ValueError(f"String '{value}' is not a number")
+
+        raise TypeError(f"Unsupported type: {type(value).__name__}. Please provide numeric value")
 
     def sum(self) -> float:
         return self.op1 + self.op2
@@ -13,18 +37,20 @@ class Calculator:
         return self.op1 - self.op2
 
     def multiply(self) -> float:
-         return self.op1 * self.op2
+        return self.op1 * self.op2
 
-    # Zwracamy informację użytkownikowi ale nie rzucamy wyjątku
-    def divide(self):
+    def divide(self) -> float | str:
         if self.op2 == 0:
-            return "Dzielenie przez zero"
+            raise ZeroDivisionError("Zero Division")
         return self.op1 / self.op2
 
-# if __name__ == '__main__':
-        # calc = Calculator(1, 2)
-    # print(calc.sum())
-    # print(calc.subtract())
-    # print(calc.multiply())
-    # print(calc.divide())
 
+if __name__ == '__main__':
+    print(Calculator(0, 2.3).sum())
+    print(Calculator(0, "2.3").sum())
+    print(Calculator(0, 2.4).subtract())
+    print(Calculator(0, 2.5).multiply())
+
+    print(Calculator(0, 0).divide())
+    print(Calculator(0, 2).divide())
+    print(Calculator(2, 0).divide())
