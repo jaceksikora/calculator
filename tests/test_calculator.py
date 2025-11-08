6af280b4f5870245b1cbe7c15325053aba84d43b
Jacek Sikora <jack.sikor@gmail.com>
2025-11-08 07:34:20 +0100

    increase tests coverage by checking invalid types

--- tests/test_calculator.py
+++ tests/test_calculator.py
@@ -3,27 +3,33 @@ import pytest
 from calc import Calculator
 
 def test_sum():
-    calc = Calculator(3, 2)
-    assert calc.sum() == 5
+    assert Calculator(3, 2).sum() == 5
 
 def test_subtract():
-    calc = Calculator(3, 2)
-    assert calc.subtract() == 1
+    assert Calculator(3, 2).subtract() == 1
 
 def test_multiply():
-    calc = Calculator(3, 2)
-    assert calc.multiply() == 6
+    assert  Calculator(3, 2).multiply() == 6
 
 def test_divide():
-    calc = Calculator(6, 2)
-    assert calc.divide() == 3
+    assert Calculator(6, 2).divide() == 3
 
 def test_divide_by_zero():
     with pytest.raises(ZeroDivisionError) as exc_info:
         Calculator(6, 0).divide()
     assert str(exc_info.value) == "Zero Division"
 
-def test_invalid_arguments():
+def test_missing_argument():
+    with pytest.raises(ValueError) as exc_info:
+        Calculator(None, 2)
+    assert str(exc_info.value) == "No value provided"
+
+def test_bool_argument():
+    with pytest.raises(TypeError) as exc_info:
+        Calculator(True, 2)
+    assert str(exc_info.value) == "Bool value is not a number"
+
+def test_string_argument():
     with pytest.raises(ValueError) as exc_info:
         Calculator("abc", 2)
     assert str(exc_info.value) == "String 'abc' is not a number"
